f949664ce5bd3b2b0e0d060b13a807e0d7eefa1a
livesammerud <46528742+livesammerud@users.noreply.github.com>
2026-03-30 22:27:13 +0200

    oppskrift

--- Script.js
+++ Script.js
@@ -17,18 +17,31 @@ function displayRecipes(recipes) {
                 <h2>${recipe.navn}</h2>
                 <div class="recipe-meta">
                     <span>⏱️ ${recipe.tid}</span>
-                    <span>🍽️ Servings: ${recipe.serveringer}</span>
+                    <span>🍽️ Serveringer: ${recipe.serveringer}</span>
                 </div>
-                
-                <h3>Ingredients:</h3>
-                <ul>
-                    ${recipe.ingredienser.map(ingredienser => `<li>${ingredienser}</li>`).join('')}
-                </ul>
-                
-                <h3>Instructions:</h3>
-                <ol>
-                    ${recipe.instruksjoner.map(step => `<li>${step}</li>`).join('')}
-                </ol>
+                <img src="images/logo.png"></img>
+                <table>
+                    <tr>
+                        <th>
+                            <h3>Ingredisenser:</h3>
+                        </th>
+                        <th class="tb_column">
+                            <h3>Instruksjoner:</h3>
+                        </th>
+                    <tr>
+                        <td>
+                            <ul>
+                            ${recipe.ingredienser.map(ingredienser => `<li>${ingredienser}</li>`).join('')}
+                            </ul>
+                        </td>
+                        <td class="tb_column">
+                            <ol>
+                            ${recipe.instruksjoner.map(step => `<li>${step}</li>`).join('')}
+                            </ol>
+                        </td>
+                    </tr>
+                    </tr>
+                </table>
             </article>
         `;
         
